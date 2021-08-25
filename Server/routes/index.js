9ec74346bc0de0a9fc96c192b27b6a046a207ada
eshitakar <eshitakar3@gmail.com>
2021-08-25 18:31:12 -0400

    Adding Vary Header

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -64,6 +64,7 @@ router.get('/room/:name', function (req, res) {
     console.log('token' + token);
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Vary', "Origin");
     //res.setHeader('Access-Control-Allow-Origin', 'https://pinmi-summer.netlify.app/');
     res.send({
       roomname: roomName,
@@ -92,6 +93,7 @@ router.get('/room/:name', function (req, res) {
       res.setHeader('Content-Type', 'application/json');
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-type, Accept");
+      res.setHeader('Vary', "Origin");
       //res.setHeader('Access-Control-Allow-Origin', 'https://pinmi-summer.netlify.app/');
       res.send({
         apiKey: apiKey,
